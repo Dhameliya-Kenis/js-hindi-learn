4893a370fa79d0648445b4d1dca425d1da987839
Dhameliya-Kenis <96010277+Dhameliya-Kenis@users.noreply.github.com>
2024-05-21 03:41:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version of js
+
+// alert("hello kenis") // we are using node.js not browser so won't be executed
+
+console.log("Kenis")
+
+let name = "kenis"
+let age = 22
+let isLoggedIn = false
+
+// number => 2^53
+// bigint =>
+// string =>""
+// boolean =>true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
