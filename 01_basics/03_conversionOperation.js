c8e6777643241c4d58535688cf05520be28a97d5
Dhameliya-Kenis <96010277+Dhameliya-Kenis@users.noreply.github.com>
2024-05-23 03:26:40 +0000

    prefix & postfix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,12 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+
+//***********Operations */
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); // based on datatypes of 1st argument it will convert all the other argument in the same data types: 
+// output: 122 // "1" is string type so other argument will be converted into the string and it will concat
+console.log(1+2+"2"); 
+// output: 32
