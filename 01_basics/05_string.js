75d63951ec8c3259b8f8f9364846961808857c6a
Dhameliya-Kenis <96010277+Dhameliya-Kenis@users.noreply.github.com>
2024-05-25 05:34:41 +0000

    string methods

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+const name = "kenis"
+const number = 25
+
+// console.log(name + number + "value")
+console.log(`my name is ${name} and number is ${number} `)
+
+let string = "kenisd"
+console.log(string.charAt(2))
+console.log(string.indexOf('e'));
