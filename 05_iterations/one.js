ce308345f3b7a70eef098f90701f9f42a3904ed9
Dhameliya-Kenis <96010277+Dhameliya-Kenis@users.noreply.github.com>
2024-05-27 04:17:47 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,16 @@
+// for loop
+
+for (let i = 0; i <= 10 ; i++) {
+    const element = i;
+    console.log(element);
+}
+
+// break & continue
+
+
+// while loop
+
+
+// do while loop -- first it will execute the loops statements and then check the condition
+
+
